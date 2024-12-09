ff22e5138233fafc4e7013d4c0d1b87d8ca9f5d0
kickstartlab <info@kickstartlabs.io>
2024-12-09 20:58:24 +0500

    update img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,36 +2,32 @@ import React from 'react'
 import icon1 from '../assets/icon1.png';
 import icon2 from '../assets/icon2.png';
 import icon3 from '../assets/icon3.png';
-import footer from '../assets/footer.png';
 import social from '../assets/social.png';
+import bottom from '../assets/bottom.jpeg';
 
 
 
 export default function Footer() {
   return (
 
-    <footer className='bg-blue-100 lg:px-20 px-5 pt-12 pb-20 lg:h-screen'>
+    <footer className='bg-blue-100 lg:px-20 px-5 lg:py-12 py-8'>
       <div className='container mx-auto'>
 
         <img src={social} alt='' className='flex mx-auto lg:w-8/12 lg:mb-24 mb-8  animate-beat'></img>
 
+        <div className='flex items-center justify-center lg:gap-x-12'>
 
-        <div>
-          <img src={footer} alt='' className='lg:w-8/12 flex mx-auto absolute left-0 right-0'></img>
+          <div className='z-10 lg:w-4/12'>
+            <img src={bottom} alt='' className='rounded-full lg:w-auto w-48  border-4 border-black-100'></img>
+          </div>
 
-          <div className='relative z-20 flex lg:-mt-12'>
-            <div className='w-1/2'>
+          <div className='bg-white-50 border-4 border-black-100 rounded-xl lg:p-10 py-5 pl-12 pr-8 lg:w-6/12 lg:space-y-8 space-y-3 lg:-translate-x-32 -translate-x-12 lg:h-auto h-36'>
+            <p className='lg:text-5xl md:text-2xl text-xl uppercase text-red-100 font-brother text-center'>become a $pump bro</p>
 
-            </div>
-
-            <div className='lg:mt-28 md:mt-16 mt-10 space-y-3 lg:-ml-12 -ml-6'>
-              <p className='md:text-5xl text-xl uppercase text-center text-red-100 font-brother'>become a $pump bro</p>
-
-              <div className='flex items-center justify-center'>
-                <a href='https://t.co/rMNr9LWk3h'><img src={icon1} alt='telegram' className='lg:w-28 w-10'></img></a>
-                <a href='https://x.com/pumpbysol'><img src={icon2} alt='twitter' className='lg:w-28 w-10'></img></a>
-                <a href='https://solscan.io/token/5oWc1d4Z6sbrH75J4jSgCCwpgpmJwkpLQbNxiP15qGAA'><img src={icon3} alt='instagram' className='lg:w-28 w-10'></img></a>
-              </div>
+            <div className='flex items-center justify-center'>
+              <a href='https://t.co/rMNr9LWk3h'><img src={icon1} alt='telegram' className='lg:w-28 w-10'></img></a>
+              <a href='https://x.com/pumpbysol'><img src={icon2} alt='twitter' className='lg:w-28 w-10'></img></a>
+              <a href='https://solscan.io/token/5oWc1d4Z6sbrH75J4jSgCCwpgpmJwkpLQbNxiP15qGAA'><img src={icon3} alt='instagram' className='lg:w-28 w-10'></img></a>
             </div>
           </div>
         </div>
